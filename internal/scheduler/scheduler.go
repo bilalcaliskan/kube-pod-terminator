dd023e49e78dc6c55261e96171592962643c313e
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-08-21 13:31:09 +0300

    change pkg to internal

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -6,7 +6,7 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"kube-pod-terminator/pkg/logging"
+	"kube-pod-terminator/internal/logging"
 	"log"
 	"sync"
 )
