d9e620690bde745a857d370c292fcf21ae2293dc
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-11-24 16:12:57 +0300

    sonarcloud integration (#18)
    * sonarcloud integration
    
    * remove sudo while running go test
    
    * add sonarcloud badge
    
    * remove windows from actions

--- internal/logging/logging_test.go
+++ internal/logging/logging_test.go
@@ -1,12 +1,14 @@
 package logging
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestGetLogger(t *testing.T) {
 	t.Log("getting logger")
 	logger := GetLogger()
+	assert.NotNil(t, logger)
 	t.Log("will try logger for debugging")
 	logger.Info("this is a test log by *zap.Logger!")
 }
--- internal/options/options_test.go
+++ internal/options/options_test.go
@@ -1,9 +1,13 @@
 package options
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestGetKubePodTerminatorOptions(t *testing.T) {
 	t.Log("fetching default options.KubePodTerminatorOptions")
 	opts := GetKubePodTerminatorOptions()
+	assert.NotNil(t, opts)
 	t.Logf("fetched default options.KubePodTerminatorOptions, %v\n", opts)
 }
